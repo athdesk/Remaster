5fcab55b4f8532b2c33c5fb490adb18011e31921
Mario Del Gaudio <mario@athdesk.me>
2023-06-13 11:27:24 +0200

    Initial Commit

---
+++ Remaster/RemasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemasterApp.swift
+//  Remaster
+//
+//  Created by Mario Del Gaudio on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RemasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
