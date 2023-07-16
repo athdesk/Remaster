766d4adde588839926b8c354a9cbcd4093c9b365
Mario Del Gaudio <mdg0000@gmail.com>
2023-07-16 13:10:13 +0200

    cleanup

--- RemasterHelperHID/Views/BasicToggleList.swift
+++ RemasterHelperHID/Views/BasicToggleList.swift
@@ -7,93 +7,10 @@
 
 import SwiftUI
 
-protocol ReprogChoice : Hashable, Identifiable {
-    func str() -> String
-}
-
-struct ReprogSelector<T: ReprogChoice, Content: View>: View {
-    let choices: [T]
-    @Binding var selection: T
-    let title: () -> Content
-    var body: some View {
-        HStack {
-            title()
-            Spacer()
-            Menu {
-                ForEach(choices, id: \.self) { s in
-                    Button(s.str()) { selection = s }
-                }
-            } label: {
-                Text(selection.str())
-            }
-            .menuStyle(.borderlessButton)
-        }
-    }
-}
-
-enum DiversionChoice: ReprogChoice, Identifiable {
-    // This is fake, but we don't use IDs with this
-    var id: ObjectIdentifier { ObjectIdentifier(Self.self) }
-    
-    case Default
-    case Diverted
-    
-    func bool() -> Bool {
-        switch self {
-        case .Default: return false
-        case .Diverted: return true
-        }
-    }
-    
-    func str() -> String {
-        switch self {
-        case .Default:
-            return "Default"
-        case .Diverted:
-            return "Controlled by Remaster"
-        }
-    }
-}
-
 struct BasicToggleList: View {
     @ObservedObject var mouse: MouseInterface
     @State private var ssSlider: Float = 0
-    
-    //TODO: bind these directly to the mouse
-    private var selVWheel: Binding<DiversionChoice> { Binding {
-        switch mouse.WheelDiversion {
-        case true:
-            return .Diverted
-        case false:
-            return .Default
-        case .none:
-            return .Default
-        case .some(_):
-            return .Default
-        }
-    } set: { v in Task {
-        if v.bool() != mouse.WheelDiversion { await mouse.toggleWheelDiversion() }
-    }}}
-    
-    private var selHWheel: Binding<DiversionChoice> { Binding {
-        switch mouse.HWheelDiversion {
-        case true:
-            return .Diverted
-        case false:
-            return .Default
-        case .none:
-            return .Default
-        case .some(_):
-            return .Default
-        }
-    } set: { v in Task {
-        if v.bool() != mouse.HWheelDiversion { await mouse.toggleHWheelDiversion() }
-    }}}
-    
-    @State private var selGestures = DiversionChoice.Default
-   
-    let divPickerChoices = [DiversionChoice.Default, DiversionChoice.Diverted]
-    
+
     var body: some View {
         if let v = mouse.Ratchet {
             Toggle(isOn: Binding(get: {v}, set: { _ in Task { await mouse.toggleRatchet() } }))
